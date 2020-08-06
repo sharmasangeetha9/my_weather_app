b920ad59508af984edcadc5f73b95fc596e67bb9
sharmasangeetha9 <sharmasangeetha9@gmail.com>
2020-08-06 21:31:52 +0100

    added relevant weather icon using API

--- src/script.js
+++ src/script.js
@@ -34,10 +34,17 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
+  let imageIcon = response.data.weather[0].icon;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${imageIcon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+let city = "London";
 
 let apiKey = "2d499af9667aee784ef86513301cd622";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
