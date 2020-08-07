f5e2b164a79fa074c0dd4df7cb5d16030ad7be4a
sharmasangeetha9 <sharmasangeetha9@gmail.com>
2020-08-07 18:15:54 +0100

    Added the search engine

--- src/script.js
+++ src/script.js
@@ -42,9 +42,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "London";
 
-let apiKey = "2d499af9667aee784ef86513301cd622";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "2d499af9667aee784ef86513301cd622";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
